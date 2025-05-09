8bf9d8341f78e5a5bd1d78c654881aad94dfb4a7
Radu Nenu <hello@radunenu.com>
2025-05-09 19:54:32 +0200

    initial hello world add

---
+++ testy/testy/testyApp.swift
@@ -0,0 +1,17 @@
+//
+//  testyApp.swift
+//  testy
+//
+//  Created by Radu Nenu on 09/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct testyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ testy/testyTests/testyTests.swift
@@ -0,0 +1,17 @@
+//
+//  testyTests.swift
+//  testyTests
+//
+//  Created by Radu Nenu on 09/05/2025.
+//
+
+import Testing
+@testable import testy
+
+struct testyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
